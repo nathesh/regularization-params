2e39752978eb4c714bbc75d9948710d5b6b429bd
nathesh <thejchess@gmail.com>
2015-03-18 13:10:42 -0500

    smaller size

--- code/classification/main.py
+++ code/classification/main.py
@@ -91,19 +91,21 @@ def data(input):  # return the data
             #data = [dt.strip_newsgroup_quoting(text) for text in data]       
             dt.add(data,0) # data and target
             target = 0
-
+        num = 0
         for f in files:
-            s_c = s + f+'/'
-            paths= os.listdir(s_c)
-            for path in paths:
-                fin = open(s_c+path, 'r') 
-                data = fin.read() # need to c
-                data = (data.decode('latin1'))
-                #data = [dt.strip_newsgroup_header(text) for text in data]
-                #data = [dt.strip_newsgroup_footer(text) for text in data]            
-                #data = [dt.strip_newsgroup_quoting(text) for text in data]
-                dt.add(data,1)
-                target = 1
+            if num <3:
+                s_c = s + f+'/'
+                paths= os.listdir(s_c)
+                for path in paths:
+                    fin = open(s_c+path, 'r') 
+                    data = fin.read() # need to c
+                    data = (data.decode('latin1'))
+                    #data = [dt.strip_newsgroup_header(text) for text in data]
+                    #data = [dt.strip_newsgroup_footer(text) for text in data]            
+                    #data = [dt.strip_newsgroup_quoting(text) for text in data]
+                    dt.add(data,1)
+                    target = 1
+            num +=1
         return dt
 
 def clean(input):  # return data, target, vectorizer and length
