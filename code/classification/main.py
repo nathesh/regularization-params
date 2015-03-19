a76f552b87b303bea5f651ec925f26e97529a578
nathesh <thejchess@gmail.com>
2015-03-19 13:12:13 -0500

    All cats

--- code/classification/main.py
+++ code/classification/main.py
@@ -87,21 +87,20 @@ def data(input):  # return the data
             data = dt.strip_newsgroup_quoting(data)
             dt.add(data, 0)  # data and target
             target = 0
-        num = 0
+        #num = 0
         for f in files:
-            if num < 3:
-                s_c = s + f + '/'
-                paths = os.listdir(s_c)
-                for path in paths:
-                    fin = open(s_c + path, 'r')
-                    data = fin.read()  # need to c
-                    data = (data.decode('latin1'))
-                    data = dt.strip_newsgroup_header(data)
-                    data = dt.strip_newsgroup_footer(data)
-                    data = dt.strip_newsgroup_quoting(data)
-                    dt.add(data, 1)
-                    target = 1
-            num += 1
+            s_c = s + f + '/'
+            paths = os.listdir(s_c)
+            for path in paths:
+                fin = open(s_c + path, 'r')
+                data = fin.read()  # need to c
+                data = (data.decode('latin1'))
+                data = dt.strip_newsgroup_header(data)
+                data = dt.strip_newsgroup_footer(data)
+                data = dt.strip_newsgroup_quoting(data)
+                dt.add(data, 1)
+                target = 1
+            #num += 1
         return dt
 
 
