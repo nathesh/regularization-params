55fcdc311fc1be1d96f17e6bfe1e1228fc5f89ad
nathesh <thejchess@gmail.com>
2015-03-29 16:29:07 -0500

    cleaning

--- code/classification/run.py
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/python
-from subprocess import call
-call(["python", "main_T.py", "0.00947373684211"])
-call(["python", "main_T.py", "0.01"])
--- code/mic/run.py
+++
@@ -1,6 +0,0 @@
-import subprocess as s 
-import numpy as np 
-a = np.linspace(.001, .00001, 60)  # input
-for alpha in a:
-   # print type(str(alpha)),alpha
-    s.call(["python","test.py",str(alpha)])
