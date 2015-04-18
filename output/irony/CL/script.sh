477d0c82b4eefd333e9b7224b5b204a8276358e1
Thejas Prasad <thejas@architect-dev>
2015-04-18 11:39:23 -0500

    ran half the trails

--- output/irony/CL/script.sh
+++ output/irony/CL/script.sh
@@ -1,9 +1,16 @@
+mkdir hinge/ log/
+for d in $(find . -type d -links 2)
+do
+   mkdir "${d}"/MAX/ "${d}"/MAJORITY/
+
+done
+
 for d in $(find . -type d -links 2)
 do
  for i in {1..10}
    do
    echo "${d}"
-   mkdir "${d}"/trails_$((i))/
+   mkdir  "${d}"/trails_$((i))/
  done
 done
 
